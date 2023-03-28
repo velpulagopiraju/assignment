f77b6a23122c606197c36ce9cea8968d485e55b9
velpulagopiraju <velpulagopiraju@gmail.com>
2023-03-28 18:00:20 +0530

    first commit

--- studentproject/student/admin.py
+++ studentproject/student/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+admin.site.register(StudentMainModel)
+admin.site.register(StudentMarksModel)
+admin.site.register(studentMarksMainModel)
+
--- studentproject/student/models.py
+++ studentproject/student/models.py
@@ -11,14 +11,14 @@ class StudentMainModel(models.Model):
     
 
 class StudentMarksModel(models.Model):
-    student = models.ForeignKey(StudentMainModel,on_delete=models.CASCADE,primary_key=True)
+    student = models.ForeignKey(StudentMainModel,on_delete=models.CASCADE)
     Marks = models.CharField(max_length=50)
     sem = models.CharField(max_length=200,choices=(('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('7','7'),('8','8'))) 
 
 
 class studentMarksMainModel(models.Model):
-    student = models.OneToOneField(StudentMainModel,on_delete=models.CASCADE,default='')
-    marks = models.ManyToManyField(StudentMarksModel)
+    student = models.OneToOneField(StudentMainModel,on_delete=models.CASCADE)
+    marks = models.ManyToManyField(StudentMarksModel,default='')
     branch = models.CharField(max_length=100,choices=(('Mech','Mech'),('csc','csc'),('ece','ece'),('it','it'),('civil','civil')))
     
     
